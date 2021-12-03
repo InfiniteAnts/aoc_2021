2d7de50f99045f625bb54d79325e8e0091894cb4
Anant <anant_ahuja@outlook.com>
2021-12-03 05:47:37 +0000

    Completed Day 1 Part 2

--- day1/main.py
+++ day1/main.py
@@ -17,7 +17,7 @@ Here are a few tips:
 Place your question data into the data.txt file.
 You may need to parse the data!
 """
-def main():
+def part_1():
   with open("data.txt") as f:
 
     data = f.readlines()
@@ -29,13 +29,27 @@ def main():
 
     print(output)
 
+def part_2():
+  with open("data.txt") as f:
+    data = f.readlines()
+
+    output = 0
+    for i in range(1, len(data) - 2):
+      first_window = int(data[i - 1]) + int(data[i]) + int(data[i + 1])
+      second_window = int(data[i]) + int(data[i + 1]) + int(data[i + 2])
+
+      if first_window < second_window:
+        output += 1
+    print(output)
+
+
 if __name__ == "__main__":
   replit_start_time = time.perf_counter()
   """
   Run your functions here.
   This code won't be run if imported.
   """
-  main()
+  part_2()
 
   # Keep this line at the end of your code
   replit_end_time = time.perf_counter()
