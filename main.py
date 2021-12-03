28b43fd6f3f59879bfbcc2a93815e57c1853ee25
Anant <anant_ahuja@outlook.com>
2021-12-03 05:30:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,42 @@
+import time
+
+"""
+Thanks for using Replit for Advent of Code!
+
+Here are a few tips:
+
+1. To install packages, just import them and Replit will install them for you, or click on the cube in the sidebar to install manually.
+2. If you're stuck, try using the debugger in the sidebar shaped like a play/pause button.
+3. When you're done, you can share your project by clicking the project name and then "Publish"
+3.a When you share your project, use the #adventofcode hashtag!
+4. Have fun, and good luck!
+"""
+
+
+"""
+Place your question data into the data.txt file.
+You may need to parse the data!
+"""
+def main():
+  with open("data.txt") as f:
+
+    data = f.readlines()
+
+    output = 0
+    for i in range(1, len(data)):
+      if int(data[i]) > int(data[i - 1]):
+        output += 1
+
+    print(output)
+
+if __name__ == "__main__":
+  replit_start_time = time.perf_counter()
+  """
+  Run your functions here.
+  This code won't be run if imported.
+  """
+  main()
+
+  # Keep this line at the end of your code
+  replit_end_time = time.perf_counter()
+  print("Elapsed time:", replit_end_time - replit_start_time)
